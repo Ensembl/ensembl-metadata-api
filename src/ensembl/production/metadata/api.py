40c9aa16f78fe44c61188aa7a45a8ebb691e6d42
James Allen <jallen@ebi.ac.uk>
2021-07-20 10:20:24 +0100

    Initial commit, updating/removing boilerplate from template

---
+++ src/ensembl/production/metadata/api.py
@@ -0,0 +1,32 @@
+import logging
+import sqlalchemy as db
+from sqlalchemy.orm import Session
+import pymysql
+
+from ensembl.production.metadata.config import MetadataRegistryConfig
+
+pymysql.install_as_MySQLdb()
+config = MetadataRegistryConfig()
+
+
+def load_database(uri=None):
+    if uri is None:
+        uri = config.METADATA_URI
+
+    try:
+        engine = db.create_engine(uri)
+    except AttributeError:
+        raise ValueError(f'Could not connect to database. Check METADATA_URI env variable.')
+
+    try:
+        connection = engine.connect()
+    except db.exc.OperationalError as err:
+        raise ValueError(f'Could not connect to database {uri}: {err}.') from err
+
+    connection.close()
+    return engine
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
