e02566fc85288381384637712a080bfff6153c5d
dpopleton <danielp@ebi.ac.uk>
2022-10-24 12:41:56 +0100

    Changed _session to use session_scope()

--- src/ensembl/production/metadata/api.py
+++ src/ensembl/production/metadata/api.py
@@ -18,11 +18,12 @@ from ensembl.database.dbconnection import DBConnection
 from ensembl.production.metadata.models import *
 
 
-#Database ORM connection.
+# Database ORM connection.
 class load_database(DBConnection):
     """
     Load a database and directly create a session for ORM interaction with the database
     """
+
     def create_session(self, engine):
         self._session = Session(engine, future=True)
 
@@ -30,13 +31,13 @@ class load_database(DBConnection):
         super().__init__(url)
         self.create_session(self._engine)
 
-    #Commit any changes to the database and create a new session instance.
+    # Commit any changes to the database and create a new session instance.
     def commit(self):
         self._session.commit()
         self._session.close()
         self.create_session(self._engine)
 
-    #rollback any changes made before commiting the session instance.
+    # rollback any changes made before committing the session instance.
     def rollback(self):
         self._session.rollback()
 
@@ -51,57 +52,57 @@ class BaseAdaptor:
     def __init__(self, metadata_uri=None):
         if metadata_uri is None:
             metadata_uri = get_metadata_uri()
-        self.metadata_db = load_database(metadata_uri)
+        self.metadata_db = DBConnection(metadata_uri)
 
 
 class ReleaseAdaptor(BaseAdaptor):
     def fetch_releases(
-        self,
-        release_id=None,
-        release_version=None,
-        current_only=True,
-        release_type=None,
-        site_name=None,
+            self,
+            release_id=None,
+            release_version=None,
+            current_only=True,
+            release_type=None,
+            site_name=None,
     ):
         release_id = check_parameter(release_id)
         release_version = check_parameter(release_version)
         release_type = check_parameter(release_type)
         site_name = check_parameter(site_name)
 
-
         release_select = db.select(
-            EnsemblRelease,EnsemblSite
+            EnsemblRelease, EnsemblSite
         ).join(EnsemblRelease.ensembl_site)
 
-        #WHERE ensembl_release.release_id = :release_id_1
+        # WHERE ensembl_release.release_id = :release_id_1
         if release_id is not None:
             release_select = release_select.filter(
                 EnsemblRelease.release_id.in_(release_id)
             )
-        #WHERE ensembl_release.version = :version_1
+        # WHERE ensembl_release.version = :version_1
         elif release_version is not None:
             release_select = release_select.filter(
                 EnsemblRelease.version.in_(release_version)
             )
-        #WHERE ensembl_release.is_current =:is_current_1
+        # WHERE ensembl_release.is_current =:is_current_1
         elif current_only:
             release_select = release_select.filter(
                 EnsemblRelease.is_current == 1
             )
 
-        #WHERE ensembl_release.release_type = :release_type_1
+        # WHERE ensembl_release.release_type = :release_type_1
         if release_type is not None:
             release_select = release_select.filter(
                 EnsemblRelease.release_type.in_(release_type)
             )
 
-        #WHERE ensembl_site.name = :name_1
+        # WHERE ensembl_site.name = :name_1
         if site_name is not None:
             release_select = release_select.filter(
                 EnsemblSite.name.in_(site_name)
             )
-        return self.metadata_db._session.execute(release_select)
-
+        with self.metadata_db.session_scope() as session:
+            session.expire_on_commit = False
+            return session.execute(release_select).all()
 
     def fetch_releases_for_genome(self, genome_uuid, site_name=None):
 
@@ -118,10 +119,11 @@ class ReleaseAdaptor(BaseAdaptor):
         )
 
         release_ids = []
-        release_objects = self.metadata_db._session.execute(release_id_select)
-        for rid in release_objects:
-            release_ids.append(rid[0])
-        release_ids = list(dict.fromkeys(release_ids))
+        with self.metadata_db.session_scope() as session:
+            release_objects = session.execute(release_id_select).all()
+            for rid in release_objects:
+                release_ids.append(rid[0])
+            release_ids = list(dict.fromkeys(release_ids))
         return self.fetch_releases(release_id=release_ids, site_name=site_name)
 
     def fetch_releases_for_dataset(self, dataset_uuid, site_name=None):
@@ -139,12 +141,14 @@ class ReleaseAdaptor(BaseAdaptor):
         )
 
         release_ids = []
-        release_objects = self.metadata_db._session.execute(release_id_select)
-        for rid in release_objects:
-            release_ids.append(rid[0])
-        release_ids = list(dict.fromkeys(release_ids))
-
+        with self.metadata_db.session_scope() as session:
+            release_objects = session.execute(release_id_select).all()
+            for rid in release_objects:
+                release_ids.append(rid[0])
+            release_ids = list(dict.fromkeys(release_ids))
         return self.fetch_releases(release_id=release_ids, site_name=site_name)
+
+
 
 
 class GenomeAdaptor(BaseAdaptor):
@@ -154,9 +158,8 @@ class GenomeAdaptor(BaseAdaptor):
         super().__init__(metadata_uri)
 
         if taxonomy_uri is None:
-            taxonomy_uri = config.TAXONOMY_URI
+            taxonomy_uri = get_taxonomy_uri()
         self.taxonomy_db = load_database(taxonomy_uri)
-        self.taxonomy_db_session = Session(self.taxonomy_db, future=True)
 
         # Cache the taxon names; data is in a separate db,
         # which is tricky to fetch elegantly and efficiently otherwise.
@@ -164,6 +167,7 @@ class GenomeAdaptor(BaseAdaptor):
         self.taxon_names = self.fetch_taxonomy_names(taxonomy_ids)
 
     def fetch_taxonomy_ids(self):
+        # No need to change this to ORM
         organism = db.Table("organism", self.md, autoload_with=self.metadata_db)
         taxonomy_id_select = db.select(organism.c.taxonomy_id.distinct())
         taxonomy_ids = [tid for (tid,) in self.metadata_db.execute(taxonomy_id_select)]
@@ -207,17 +211,17 @@ class GenomeAdaptor(BaseAdaptor):
         return taxons
 
     def fetch_genomes(
-        self,
-        genome_id=None,
-        genome_uuid=None,
-        assembly_accession=None,
-        ensembl_name=None,
-        taxonomy_id=None,
-        unreleased_only=False,
-        site_name=None,
-        release_type=None,
-        release_version=None,
-        current_only=True,
+            self,
+            genome_id=None,
+            genome_uuid=None,
+            assembly_accession=None,
+            ensembl_name=None,
+            taxonomy_id=None,
+            unreleased_only=False,
+            site_name=None,
+            release_type=None,
+            release_version=None,
+            current_only=True,
     ):
         genome_id = check_parameter(genome_id)
         genome_uuid = check_parameter(genome_uuid)
@@ -310,13 +314,13 @@ class GenomeAdaptor(BaseAdaptor):
             yield result_dict
 
     def fetch_genomes_by_genome_uuid(
-        self,
-        genome_uuid,
-        unreleased_only=False,
-        site_name=None,
-        release_type=None,
-        release_version=None,
-        current_only=True,
+            self,
+            genome_uuid,
+            unreleased_only=False,
+            site_name=None,
+            release_type=None,
+            release_version=None,
+            current_only=True,
     ):
         return self.fetch_genomes(
             genome_uuid=genome_uuid,
@@ -328,13 +332,13 @@ class GenomeAdaptor(BaseAdaptor):
         )
 
     def fetch_genomes_by_assembly_accession(
-        self,
-        assembly_accession,
-        unreleased_only=False,
-        site_name=None,
-        release_type=None,
-        release_version=None,
-        current_only=True,
+            self,
+            assembly_accession,
+            unreleased_only=False,
+            site_name=None,
+            release_type=None,
+            release_version=None,
+            current_only=True,
     ):
         return self.fetch_genomes(
             assembly_accession=assembly_accession,
@@ -346,13 +350,13 @@ class GenomeAdaptor(BaseAdaptor):
         )
 
     def fetch_genomes_by_ensembl_name(
-        self,
-        ensembl_name,
-        unreleased_only=False,
-        site_name=None,
-        release_type=None,
-        release_version=None,
-        current_only=True,
+            self,
+            ensembl_name,
+            unreleased_only=False,
+            site_name=None,
+            release_type=None,
+            release_version=None,
+            current_only=True,
     ):
         return self.fetch_genomes(
             ensembl_name=ensembl_name,
@@ -364,13 +368,13 @@ class GenomeAdaptor(BaseAdaptor):
         )
 
     def fetch_genomes_by_taxonomy_id(
-        self,
-        taxonomy_id,
-        unreleased_only=False,
-        site_name=None,
-        release_type=None,
-        release_version=None,
-        current_only=True,
+            self,
+            taxonomy_id,
+            unreleased_only=False,
+            site_name=None,
+            release_type=None,
+            release_version=None,
+            current_only=True,
     ):
         return self.fetch_genomes(
             taxonomy_id=taxonomy_id,
@@ -382,13 +386,13 @@ class GenomeAdaptor(BaseAdaptor):
         )
 
     def fetch_genomes_by_scientific_name(
-        self,
-        scientific_name,
-        unreleased_only=False,
-        site_name=None,
-        release_type=None,
-        release_version=None,
-        current_only=True,
+            self,
+            scientific_name,
+            unreleased_only=False,
+            site_name=None,
+            release_type=None,
+            release_version=None,
+            current_only=True,
     ):
         taxonomy_ids = [
             t_id
@@ -406,13 +410,13 @@ class GenomeAdaptor(BaseAdaptor):
         )
 
     def fetch_genomes_by_synonym(
-        self,
-        synonym,
-        unreleased_only=False,
-        site_name=None,
-        release_type=None,
-        release_version=None,
-        current_only=True,
+            self,
+            synonym,
+            unreleased_only=False,
+            site_name=None,
+            release_type=None,
+            release_version=None,
+            current_only=True,
     ):
         taxonomy_ids = []
         for taxon_id in self.taxon_names:
@@ -431,11 +435,11 @@ class GenomeAdaptor(BaseAdaptor):
         )
 
     def fetch_sequences(
-        self,
-        genome_id=None,
-        genome_uuid=None,
-        assembly_accession=None,
-        chromosomal_only=False,
+            self,
+            genome_id=None,
+            genome_uuid=None,
+            assembly_accession=None,
+            chromosomal_only=False,
     ):
         genome_id = check_parameter(genome_id)
         genome_uuid = check_parameter(genome_uuid)
@@ -489,7 +493,7 @@ class GenomeAdaptor(BaseAdaptor):
         )
 
     def fetch_sequences_by_assembly_accession(
-        self, assembly_accession, chromosomal_only=False
+            self, assembly_accession, chromosomal_only=False
     ):
         return self.fetch_sequences(
             assembly_accession=assembly_accession, chromosomal_only=chromosomal_only
--- tests/test_api.py
+++ tests/test_api.py
@@ -12,30 +12,28 @@
 """
 Unit tests for api module
 """
-from os.path import dirname
 from ensembl.production.metadata.api import *
 
-DB_NAME = 'sqlite:///' + dirname(__file__) + '/TEST.db'
-
 def test_load_database():
-    DB_TEST = ReleaseAdaptor(DB_NAME)
+    DB_TEST = ReleaseAdaptor('sqlite:///TEST.db')
     assert DB_TEST, "DB should not be empty"
 
 def test_fetch_releases():
-    conn = ReleaseAdaptor(DB_NAME)
-    TEST = conn.fetch_releases(release_id=1).one()
+    conn = ReleaseAdaptor('sqlite:///TEST.db')
+    TEST = conn.fetch_releases(release_id=1)
     #Test the one to many connection
-    assert TEST.EnsemblSite.name == '2020-map'
+    print (TEST)
+    assert TEST[0].EnsemblSite.name == '2020-map'
     #Test the direct access.
-    assert TEST.EnsemblRelease.label == '2020 MAP 7 species'
+    assert TEST[0].EnsemblRelease.label == '2020 MAP 7 species'
 
 #currently only have one release, so the testing is not comprehensive
 def test_fetch_releases_for_genome():
-    conn = ReleaseAdaptor(DB_NAME)
-    TEST = conn.fetch_releases_for_genome('a733574a-93e7-11ec-a39d-005056b38ce3').one()
-    assert TEST.EnsemblSite.name == '2020-map'
+    conn = ReleaseAdaptor('sqlite:///TEST.db')
+    TEST = conn.fetch_releases_for_genome('a733574a-93e7-11ec-a39d-005056b38ce3')
+    assert TEST[0].EnsemblSite.name == '2020-map'
 
 def test_fetch_releases_for_dataset():
-    conn = ReleaseAdaptor(DB_NAME)
-    TEST = conn.fetch_releases_for_dataset('76ffa505-948d-11ec-a39d-005056b38ce3').one()
-    assert TEST.EnsemblSite.name == '2020-map'
+    conn = ReleaseAdaptor('sqlite:///TEST.db')
+    TEST = conn.fetch_releases_for_dataset('76ffa505-948d-11ec-a39d-005056b38ce3')
+    assert TEST[0].EnsemblSite.name == '2020-map'
