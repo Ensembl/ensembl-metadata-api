0ee9019af58522f5b9a19b7fe97cdbf698c22a5b
vinay <vinay@ebi.ac.uk>
2023-04-27 13:52:48 +0100

    group param added to fetch_genomes

--- src/ensembl/production/metadata/api.py
+++ src/ensembl/production/metadata/api.py
@@ -230,6 +230,7 @@ class GenomeAdaptor(BaseAdaptor):
             assembly_accession=None,
             ensembl_name=None,
             taxonomy_id=None,
+            group=None,
             unreleased_only=False,
             site_name=None,
             release_type=None,
@@ -241,11 +242,18 @@ class GenomeAdaptor(BaseAdaptor):
         assembly_accession = check_parameter(assembly_accession)
         ensembl_name = check_parameter(ensembl_name)
         taxonomy_id = check_parameter(taxonomy_id)
+        group = check_parameter(group)
 
         genome_select = db.select(
-            Genome, Organism, Assembly
+          Genome, Organism, Assembly
         ).join(Genome.assembly).join(Genome.organism)
-
+        
+        if group :
+          genome_select = db.select(
+              Genome, Organism, Assembly, OrganismGroupMember, OrganismGroup
+          ).join(Genome.assembly).join(Genome.organism).join(
+            Organism.organism_group_members).join(OrganismGroupMember.organism_group).filter(OrganismGroup.type == 'division').filter(OrganismGroup.name.in_(group))
+            
         if unreleased_only:
             genome_select = genome_select.outerjoin(Genome.genome_releases).filter(
                 GenomeRelease.genome_id == None
@@ -424,7 +432,4 @@ class GenomeAdaptor(BaseAdaptor):
         return self.fetch_sequences(
             assembly_accession=assembly_accession, chromosomal_only=chromosomal_only
         )
-
-
-
-
+        
