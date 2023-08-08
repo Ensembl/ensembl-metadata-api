fad763c013f599b1a4c1365b80b7d2306e1a7ac7
danielp <danielp@ebi.ac.uk>
2023-08-08 09:53:30 +0100

    added a change for taxid check. Major rework already in PR so it isn't quite proper

--- src/ensembl/production/metadata/api/genome.py
+++ src/ensembl/production/metadata/api/genome.py
@@ -29,7 +29,7 @@ class GenomeAdaptor(BaseAdaptor):
         self.taxonomy_db = DBConnection(taxonomy_uri)
 
     def fetch_taxonomy_names(self, taxonomy_ids):
-        if not isinstance(taxonomy_ids, str):
+        if taxonomy_ids.isdigit():
             taxonomy_ids = check_parameter(taxonomy_ids)
         taxons = {}
         for tid in taxonomy_ids:
