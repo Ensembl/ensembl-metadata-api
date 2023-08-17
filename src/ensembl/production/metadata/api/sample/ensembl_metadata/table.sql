3694b3eb13c1fbc561bdba6d5d6ccf1bd8a03118
danielp <danielp@ebi.ac.uk>
2023-08-17 12:59:47 +0100

    Test DB fixes

--- src/ensembl/production/metadata/api/sample/ensembl_metadata/table.sql
+++ src/ensembl/production/metadata/api/sample/ensembl_metadata/table.sql
@@ -1,18 +1,18 @@
 CREATE TABLE assembly
 (
     assembly_id      int auto_increment primary key,
-    assembly_uuid     varchar(128) not null,
-    ucsc_name        varchar(16)  null,
+    assembly_uuid    varchar(128) not null,
+    ucsc_name        varchar(16) null,
     accession        varchar(16)  not null,
     level            varchar(32)  not null,
     name             varchar(128) not null,
-    accession_body   varchar(32)  null,
-    assembly_default varchar(32)  null,
-    tol_id           varchar(32)  null,
-    created          datetime     null,
+    accession_body   varchar(32) null,
+    assembly_default varchar(32) null,
+    tol_id           varchar(32) null,
+    created          datetime null,
     ensembl_name     varchar(255) null,
-    alt_accession    varchar(16)  null,
-    is_reference     tinyint(1)   not null,
+    alt_accession    varchar(16) null,
+    is_reference     tinyint(1) not null,
     url_name         varchar(128) null,
 
     constraint assembly_uuid
@@ -30,14 +30,14 @@ CREATE TABLE assembly_sequence
     assembly_sequence_id int auto_increment
         primary key,
     name                 varchar(128) null,
-    assembly_id          int          not null,
+    assembly_id          int not null,
     accession            varchar(128) null,
-    chromosomal          tinyint(1)   not null,
-    length               int          not null,
-    chromosome_rank      int          null,
-    sequence_location    varchar(10)  null,
-    md5                  varchar(32)  null,
-    sha512t4u            varchar(128)  null,
+    chromosomal          tinyint(1) not null,
+    length               int not null,
+    chromosome_rank      int null,
+    sequence_location    varchar(10) null,
+    md5                  varchar(32) null,
+    sha512t4u            varchar(128) null,
     constraint assembly_sequence_assembly_id_accession_5f3e5119_uniq
         unique (assembly_id, accession),
     constraint assembly_sequence_assembly_id_2a84ddcb_fk_assembly_assembly_id
@@ -51,9 +51,9 @@ CREATE TABLE attribute
 (
     attribute_id int auto_increment
         primary key,
-    name         varchar(128)                                                          not null,
-    label        varchar(128)                                                          not null,
-    description  varchar(255)                                                          null,
+    name         varchar(128) not null,
+    label        varchar(128) not null,
+    description  varchar(255) null,
     type         enum ('integer', 'float', 'percent', 'string', 'bp') default 'string' null,
     constraint name
         unique (name),
@@ -88,13 +88,13 @@ CREATE TABLE dataset
 (
     dataset_id        int auto_increment
         primary key,
-    dataset_uuid      varchar(128)                                                       not null,
-    dataset_type_id   int                                                                not null,
-    name              varchar(128)                                                       not null,
-    version           varchar(128)                                                       null,
-    created           datetime(6)                                                        not null,
-    dataset_source_id int                                                                not null,
-    label             varchar(128)                                                       not null,
+    dataset_uuid      varchar(128) not null,
+    dataset_type_id   int          not null,
+    name              varchar(128) not null,
+    version           varchar(128) null,
+    created           datetime(6) not null,
+    dataset_source_id int          not null,
+    label             varchar(128) not null,
     status            enum ('Submitted', 'Progressing', 'Processed') default 'Submitted' null,
     constraint dataset_uuid
         unique (dataset_uuid),
@@ -140,9 +140,9 @@ CREATE TABLE ensembl_release
         primary key,
     version      decimal(10, 1) not null,
     release_date date           not null,
-    label        varchar(64)    null,
-    is_current   tinyint(1)     not null,
-    site_id      int            null,
+    label        varchar(64) null,
+    is_current   tinyint(1) not null,
+    site_id      int null,
     release_type varchar(16)    not null,
     constraint ensembl_release_version_site_id_b743399a_uniq
         unique (version, site_id),
@@ -156,7 +156,7 @@ CREATE TABLE organism
         primary key,
     organism_uuid            varchar(128) not null,
     taxonomy_id              int          not null,
-    species_taxonomy_id      int          null,
+    species_taxonomy_id      int null,
     common_name              varchar(128) not null,
     strain                   varchar(128) null,
     scientific_name          varchar(128) null,
@@ -176,7 +176,7 @@ CREATE TABLE genome
     genome_uuid varchar(128) not null,
     assembly_id int          not null,
     organism_id int          not null,
-    created     datetime(6)  not null,
+    created     datetime(6) not null,
     constraint genome_uuid
         unique (genome_uuid),
     constraint genome_assembly_id_0a748388_fk_assembly_assembly_id
@@ -189,9 +189,9 @@ CREATE TABLE genome_dataset
 (
     genome_dataset_id int auto_increment
         primary key,
-    dataset_id        int        not null,
-    genome_id         int        not null,
-    release_id        int        null,
+    dataset_id        int not null,
+    genome_id         int not null,
+    release_id        int null,
     is_current        tinyint(1) not null,
     constraint ensembl_metadata_gen_dataset_id_26d7bac7_fk_dataset_d
         foreign key (dataset_id) references dataset (dataset_id),
@@ -205,8 +205,8 @@ CREATE TABLE genome_release
 (
     genome_release_id int auto_increment
         primary key,
-    genome_id         int        not null,
-    release_id        int        not null,
+    genome_id         int not null,
+    release_id        int not null,
     is_current        tinyint(1) not null,
     is_best           tinyint(1) not null,
     constraint genome_release_genome_id_3e45dc04_fk
@@ -221,25 +221,23 @@ CREATE TABLE organism_group
         primary key,
     type              varchar(32)  not null,
     name              varchar(255) not null,
-    code              varchar(48)  null,
+    code              varchar(48) null,
     constraint group_type_name_63c2f6ac_uniq
         unique (type, name),
     constraint organism_group_code_uindex
         unique (code)
 );
 
-CREATE TABLE organism_group_member
+CREATE TABLE `organism_group_member`
 (
-    organism_group_member_id int auto_increment
-        primary key,
-    is_reference             tinyint(1) not null,
-    order                    int        null,
-    organism_id              int        not null,
-    organism_group_id        int        not null,
-    constraint organism_group_member_organism_id_organism_gro_fe8f49ac_uniq
-        unique (organism_id, organism_group_id),
-    constraint organism_group_membe_organism_group_id_533ca128_fk_organism_
-        foreign key (organism_group_id) references organism_group (organism_group_id),
-    constraint organism_group_membe_organism_id_2808252e_fk_organism_
-        foreign key (organism_id) references organism (organism_id)
+    `organism_group_member_id` int NOT NULL AUTO_INCREMENT,
+    `is_reference`             tinyint(1) DEFAULT NULL,
+    `organism_id`              int NOT NULL,
+    `organism_group_id`        int NOT NULL,
+    `order`                    int DEFAULT NULL,
+    PRIMARY KEY (`organism_group_member_id`),
+    UNIQUE KEY `organism_group_member_organism_id_organism_gro_fe8f49ac_uniq` (`organism_id`,`organism_group_id`),
+    KEY                        `organism_group_membe_organism_group_id_533ca128_fk_organism_` (`organism_group_id`),
+    CONSTRAINT `organism_group_membe_organism_group_id_533ca128_fk_organism_` FOREIGN KEY (`organism_group_id`) REFERENCES `organism_group` (`organism_group_id`),
+    CONSTRAINT `organism_group_membe_organism_id_2808252e_fk_organism_` FOREIGN KEY (`organism_id`) REFERENCES `organism` (`organism_id`)
 );
