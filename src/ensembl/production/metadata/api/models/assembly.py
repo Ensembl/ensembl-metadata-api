1fb266b2af8882f102755df9a11fd037e6ec44aa
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2023-10-27 12:43:43 +0100

    Compatibility with other code.

--- src/ensembl/production/metadata/api/models/assembly.py
+++ src/ensembl/production/metadata/api/models/assembly.py
@@ -21,25 +21,25 @@ from ensembl.production.metadata.api.models.base import Base
 class Assembly(Base):
     __tablename__ = 'assembly'
 
-    assembly_id = Column(Integer, primary_key=True)
-    assembly_uuid = Column(String(128), unique=True, nullable=False, default=uuid.uuid4)
-    ucsc_name = Column(String(16))
-    accession = Column(String(16), nullable=False, unique=True)
-    level = Column(String(32), nullable=False)
-    name = Column(String(128), nullable=False)
-    accession_body = Column(String(32))
-    assembly_default = Column(String(128))
-    tol_id = Column(String(32), unique=True)
-    created = Column(DateTime)
-    ensembl_name = Column(String(255), unique=True)
-    alt_accession = Column(String(16), nullable=True)
-    is_reference = Column(TINYINT(1), nullable=False, default=0)
-    url_name = Column(String(128), nullable=False)
-    # One to many relationships
-    # assembly_id within assembly_sequence
-    assembly_sequences = relationship("AssemblySequence", back_populates="assembly", cascade="all, delete, delete-orphan")
-    # assembly_id within genome
-    genomes = relationship("Genome", back_populates="assembly", cascade="all, delete, delete-orphan")
+	assembly_id = Column(Integer, primary_key=True)
+	assembly_uuid = Column(String(128), unique=True, nullable=False, default=uuid.uuid4)
+	ucsc_name = Column(String(16))
+	accession = Column(String(16), nullable=False, unique=True)
+	level = Column(String(32), nullable=False)
+	name = Column(String(128), nullable=False)
+	accession_body = Column(String(32))
+	assembly_default = Column(String(128))
+	tol_id = Column(String(32), unique=True)
+	created = Column(DateTime)
+	ensembl_name = Column(String(255), unique=True)
+	alt_accession = Column(String(16), nullable=True)
+	is_reference = Column(TINYINT(1), nullable=False, default=0)
+	url_name = Column(String(128), nullable=False)
+	# One to many relationships
+	# assembly_id within assembly_sequence
+	assembly_sequences = relationship("AssemblySequence", back_populates="assembly", cascade="all, delete, delete-orphan")
+	# assembly_id within genome
+	genomes = relationship("Genome", back_populates="assembly", cascade="all, delete, delete-orphan")
 
 
 class AssemblySequence(Base):
@@ -48,19 +48,24 @@ class AssemblySequence(Base):
         Index('assembly_sequence_assembly_id_accession_5f3e5119_uniq', 'assembly_id', 'accession', unique=True),
     )
 
-    assembly_sequence_id = Column(Integer, primary_key=True)
-    name = Column(String(128), unique=True)
-    assembly_id = Column(ForeignKey('assembly.assembly_id'), nullable=False, index=True)
-    accession = Column(String(128), nullable=False)
-    chromosomal = Column(TINYINT(1), nullable=False, default=0)
-    chromosome_rank = Column(Integer)
-    length = Column(Integer, nullable=False)
-    sequence_location = Column(String(10))
-    md5 = Column(String(32))
-    sha512t24u = Column(String(128))
-    # One to many relationships
-    # none
-    # many to one relationships
-    # assembly_id within assembly
-    assembly = relationship('Assembly', back_populates="assembly_sequences")
+	assembly_sequence_id = Column(Integer, primary_key=True)
+	name = Column(String(128), unique=True)
+	assembly_id = Column(ForeignKey('assembly.assembly_id'), nullable=False, index=True)
+	accession = Column(String(128), nullable=False)
+	chromosomal = Column(TINYINT(1), nullable=False, default=0)
+	chromosome_rank = Column(Integer)
+	length = Column(Integer, nullable=False)
+	sequence_location = Column(String(10))
+	md5 = Column(String(32))
+	# column need renaming as well
+	sha512t24u = Column(String(128))
+	assembly = relationship('Assembly', back_populates="assembly_sequences")
 
+	# backward compatibility with old column name sha512t2u
+	@property
+	def sha512t2u(self):
+		return self.sha512t24u
+
+	@sha512t2u.setter
+	def sha512t2u(self, checksum):
+		self.sha512t24u = checksum
