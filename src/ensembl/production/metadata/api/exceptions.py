f3699f13ca514fff172e34476d0142d30ea4f084
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2023-12-12 11:00:33 +0000

    Update src/ensembl/production/metadata/api/exceptions.py

--- src/ensembl/production/metadata/api/exceptions.py
+++ src/ensembl/production/metadata/api/exceptions.py
@@ -9,10 +9,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import sqlalchemy as db
-from sqlalchemy import inspect
-from sqlalchemy.engine import make_url
-
 
 class MetaException(Exception):
     """Base Metadata API Exception class"""
