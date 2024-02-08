2ec24caedee871880811b424f207a54d5420c82f
danielp <danielp@ebi.ac.uk>
2024-02-08 09:37:56 +0000

    Initial function definitions

--- src/ensembl/production/metadata/api/hive/dataset_factory.py
+++ src/ensembl/production/metadata/api/hive/dataset_factory.py
@@ -2,7 +2,7 @@ from ensembl.production.metadata.api.exceptions import *
 
 class DatasetFactory():
     """
-    A class used to interact with the dataset REST endpoint and to add data.
+    A class used to interact with the ensembl_genome_metadata to modify  dataset and dataset attribute table.
 
     ...
 
@@ -14,23 +14,26 @@ class DatasetFactory():
     -------
     get_child_datasets()
     """
-    def __init__(self, genome_uuid=None,dataset_type=None,dataset_uuid=None):
-        if genome_uuid == None and dataset_uuid == None:
-            raise DatasetFactoryException("genome_uuid + datset.type or dataset_uuid are required")
-
-    def get_child_datasets(self):
+    # def __init__(self):
+    #     #TODO: Determine how to implement genome_uuid when we can have multiples of each dataset type per genome
+    def get_child_datasets(self, dataset_uuid=None):
         #Function to get all of the possible children datasets that are not constrained
-
-    def create_child_datasets(self):
+        #Only returns children of dataset_uuid if specified
+        child_datasets = []
+        return child_datasets
+    def create_child_datasets(self, dataset_uuid=None, dataset_type=None):
         #Recursive function to create all the child datasets that it can. Breaks when no more datasets are created
-        print "Not Implemented"
+        #Only returns children of dataset_uuid if specified
+        #Should be limited to a single type if dataset_uuid is not specified
+        child_datasets = self.get_child_datasets()
+        return child_datasets
 
-    def create_dataset(self):
-        print "Not Implemented"
+    def create_dataset(self,genome_uuid, datasource, dataset_type, dataset_attributes):
+        dataset_uuid = ''
+        return dataset_uuid
+    def update_dataset_status(self,dataset_uuid,status):
+        return dataset_uuid,status
 
-    def update_dataset_status(self):
-        print "Not Implemented"
-
-    def update_dataset_attributes(self):
-        print "Not Implemented"
-
+    def update_dataset_attributes(self,dataset_uuid, dataset_attributes):
+        datset_attribute_indicies = []
+        return dataset_uuid,datset_attribute_indicies
