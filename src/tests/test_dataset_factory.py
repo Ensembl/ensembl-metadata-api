2d51ac4fdf3b589630d615e249c3f24aa7dce78b
danielp <danielp@ebi.ac.uk>
2024-02-28 14:32:24 +0000

    Renamed and moved files

--- src/tests/test_dataset_factory.py
+++ src/tests/test_dataset_factory.py
@@ -10,9 +10,11 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 from pathlib import Path
+
 import pytest
 from ensembl.database import UnitTestDB, DBConnection
-from ensembl.production.metadata.api.hive.dataset_factory import DatasetFactory
+from ensembl.production.metadata.api.factories.dataset_factory import DatasetFactory
+
 from ensembl.production.metadata.api.models import (Dataset, DatasetAttribute, Attribute, DatasetSource, DatasetType,
                                                     DatasetStatus, GenomeDataset, Genome)
 
