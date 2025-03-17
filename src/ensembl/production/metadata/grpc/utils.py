63a669de63e89ac6975a398391542b14a8d8637a
Bilal <bilal@ebi.ac.uk>
2025-03-17 13:39:57 +0000

    :sparkles: push the initial logic (WIP)

--- src/ensembl/production/metadata/grpc/utils.py
+++ src/ensembl/production/metadata/grpc/utils.py
@@ -268,6 +268,35 @@ def get_genome_by_uuid(db_conn, genome_uuid, release_version):
     return msg_factory.create_genome()
 
 
+def get_newest_genome(db_conn, current_genome):
+    # current_genome_uuid = current_genome.Genome.genome_uuid,
+    current_genome_release_date = current_genome.EnsemblRelease.release_date,
+    assembly_name = current_genome.Assembly.name
+    logger.debug(f"Current genome release date: '{current_genome_release_date}', assembly_name: '{assembly_name}'")
+
+    # We get all genomes belonging to `assembly_name` sorted by release_date (newest first)
+    all_genomes_results = db_conn.fetch_genomes(assembly_name=assembly_name)
+
+    if len(all_genomes_results) > 0:
+        print(f"List of all genomes' UUIDs found with their release info belonging to '{assembly_name}' assembly:")
+        print(
+            "\n".join(
+                f"Genome: {result.Genome.genome_uuid}, "
+                f"Release Date: {result.EnsemblRelease.release_date}, " 
+                f"Release Type: {result.EnsemblRelease.release_type} "
+                for result in all_genomes_results
+            )
+        )
+        # Now we need to check if the current_genome_release_date is the newest
+        if all_genomes_results[0].EnsemblRelease.release_date == current_genome_release_date:
+            logger.debug(f"The current provided genome is the newest")
+            return None
+        # or not, in this case we return the newest genome :)
+        logger.debug(f"The newest genome's release date: '{all_genomes_results[0].EnsemblRelease.release_date}'")
+        return all_genomes_results[0]
+
+    return None
+
 def get_brief_genome_details_by_uuid(db_conn, genome_uuid_or_tag, release_version):
     """
     Fetch brief genome details by UUID or tag and release version.
@@ -312,8 +341,16 @@ def get_brief_genome_details_by_uuid(db_conn, genome_uuid_or_tag, release_versio
             f"Multiple results found for Genome UUID/Release version: {genome_uuid_or_tag}/{release_version}. "
             f"Using the first result.")
 
+    # Once the whole fetching process is done, and before passing the result to create_brief_genome_details
+    # We check if the chosen genome has a newer version, that logic goes here
+    newest_genome = get_newest_genome( # if any
+        db_conn=db_conn,
+        current_genome=genome_results[0]
+    )
+    print(f"newest_genome ---> {newest_genome}")
+
     # Return the first result
-    return msg_factory.create_brief_genome_details(data=genome_results[0])
+    return msg_factory.create_brief_genome_details(genome_results[0], newest_genome)
 
 def get_attributes_by_genome_uuid(db_conn, genome_uuid, release_version):
     if not genome_uuid:
