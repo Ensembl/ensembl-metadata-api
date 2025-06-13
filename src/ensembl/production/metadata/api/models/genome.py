d71318ef0610d3a1ccfa4db295f0b776828de969
danielp <danielp@ebi.ac.uk>
2025-06-13 16:37:26 +0100

    Moved get public path from genome model to genome adaptor. Removed ensembl field. Updated tests.

--- src/ensembl/production/metadata/api/models/genome.py
+++ src/ensembl/production/metadata/api/models/genome.py
@@ -10,14 +10,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 import logging
-import re
 import uuid
 
 from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint
 from sqlalchemy.dialects.mysql import DATETIME, TINYINT
 from sqlalchemy.orm import relationship
 
-from ensembl.production.metadata.api.exceptions import *
 from ensembl.production.metadata.api.models.base import Base, LoadAble
 
 __all__ = ['Genome', 'GenomeDataset', 'GenomeRelease']
@@ -47,88 +45,6 @@ class Genome(LoadAble, Base):
     # organism_id to organism
     organism = relationship("Organism", back_populates="genomes")
 
-    def get_public_path(self, dataset_type='all', release=None):
-        # TODO manage the Release parameter to fetch datasets attached to release anterior to the one specified.
-        paths = []
-        genome_genebuild_dataset = next(
-            (gd for gd in self.genome_datasets if gd.dataset.dataset_type.name == "genebuild"),
-            None)
-        if genome_genebuild_dataset is None:
-            raise ValueError("Genebuild dataset not found for the genome")
-        genebuild_dataset = genome_genebuild_dataset.dataset
-        genebuild_source_name = next(
-            (da.value for da in genebuild_dataset.dataset_attributes if
-             da.attribute.name == "genebuild.annotation_source"),
-            'ensembl')
-        # Genebuild version is either the laste_geneset_update or the start_date if not specified.
-        root_datasets = [ds for ds in self.genome_datasets if ds.dataset.parent_id is None]
-        try:
-            match = re.match(r'^(\d{4}-\d{2})', genebuild_dataset.genebuild_version)
-            genebuild_version = match.group(1).replace('-', '_')
-        except TypeError as e:
-            logger.fatal(f"For genome {self.genome_uuid}, can't find genebuild_version directory")
-            raise RuntimeError(e)
-        base_path = f"{self.organism.scientific_name.replace(' ', '_')}/{self.assembly.accession}"
-        common_path = f"{base_path}/{genebuild_source_name}"
-        
-        unique_dataset_types = {gd.dataset.dataset_type.name for gd in root_datasets}
-        if 'vep_genome_feature' in unique_dataset_types or 'vep_assembly_feature' in unique_dataset_types:
-            unique_dataset_types.discard('vep_genome_feature')
-            unique_dataset_types.discard('vep_assembly_feature')
-        if 'regulatory_features' in unique_dataset_types or 'regulation_build' in unique_dataset_types:
-            unique_dataset_types.discard('regulatory_features')
-            unique_dataset_types.discard('regulation_build')
-            unique_dataset_types.add('regulation')
-        if 'evidence' in unique_dataset_types:
-            unique_dataset_types.discard('evidence')
-            unique_dataset_types.add('variation')
-        if 'regulatory_features' == dataset_type or 'regulation_build' == dataset_type:
-            dataset_type = 'regulation'
-
-        # if any of these three values are in unique_dataset_types
-        discarded_homology_types = {'homology_load', 'homology_compute', 'homology_ftp'}
-        if unique_dataset_types.intersection(discarded_homology_types):
-            # discard them
-            unique_dataset_types.difference_update(discarded_homology_types)
-            # and add 'homologies' if it doesn't exist
-            unique_dataset_types.add('homologies')
-
-        discarded_web_types = {'web_genesearch', 'web_genomediscovery'}
-        if unique_dataset_types.intersection(discarded_web_types):
-            # we are just discarding them for now
-            unique_dataset_types.difference_update(discarded_web_types)
-
-        # Defining path templates
-        path_templates = {
-            'genebuild': f"{common_path}/geneset/{genebuild_version}",
-            'assembly': f"{base_path}/genome",
-            'homologies': f"{common_path}/homology/{genebuild_version}",
-            'regulation': f"{common_path}/regulation",
-            'variation': f"{common_path}/variation/{genebuild_version}",
-        }
-
-        # Check for invalid dataset type early
-        if dataset_type not in unique_dataset_types and dataset_type != 'all':
-            raise TypeNotFoundException(f"Dataset Type : {dataset_type} not found in metadata.")
-
-        # If 'all', add paths for all unique dataset types
-        if dataset_type == 'all':
-            for t in unique_dataset_types:
-                paths.append({
-                    "dataset_type": t,
-                    "path": path_templates[t]
-                })
-        elif dataset_type in path_templates:
-            # Add path for the specific dataset type
-            paths.append({
-                "dataset_type": dataset_type,
-                "path": path_templates[dataset_type]
-            })
-        else:
-            # If the code reaches here, it means there is a logic error
-            raise TypeNotFoundException(f"Dataset Type : {dataset_type} has no associated path.")
-
-        return paths
 
 
 class GenomeDataset(LoadAble, Base):
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -16,11 +16,9 @@ Unit tests for api module
 from pathlib import Path
 
 import pytest
-from ensembl.utils.database import DBConnection
 from ensembl.utils.database import UnitTestDB
 
-from ensembl.production.metadata.api.exceptions import TypeNotFoundException
-from ensembl.production.metadata.api.models import Organism, Genome
+from ensembl.production.metadata.api.adaptors.genome import *
 
 
 @pytest.mark.parametrize("test_dbs", [[{"src": Path(__file__).parent / "databases/ensembl_genome_metadata"},
@@ -29,50 +27,37 @@ class TestApi:
     dbc = None  # type: UnitTestDB
 
     def test_get_public_path(self, test_dbs):
-        metadata_db = DBConnection(test_dbs['ensembl_genome_metadata'].dbc.url)
-        with metadata_db.session_scope() as session:
-            genome = session.query(Genome).filter(Genome.genome_uuid == 'a733574a-93e7-11ec-a39d-005056b38ce3').first()
-            paths = genome.get_public_path(dataset_type='all')
-            assert len(paths) == 4
-            # assert all("/genebuild/" in path for path in paths)
-            path = genome.get_public_path(dataset_type='genebuild')
-            assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/community/geneset/2018_10'
-            path = genome.get_public_path(dataset_type='assembly')
-            assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/genome'
-            path = genome.get_public_path(dataset_type='variation')
-            assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/community/variation/2018_10'
-            path = genome.get_public_path(dataset_type='homologies')
-            assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/community/homology/2018_10'
-            with pytest.raises(TypeNotFoundException):
-                genome.get_public_path(dataset_type='regulatory_features')
-                # assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/ensembl/regulation'
+        genome_adapter = GenomeAdaptor(test_dbs['ensembl_genome_metadata'].dbc.url, test_dbs['ncbi_taxonomy'].dbc.url)
+        genome_uuid = 'a733574a-93e7-11ec-a39d-005056b38ce3'
+        paths = genome_adapter.get_public_path(genome_uuid, dataset_type='all')
+        assert len(paths) == 4
+        # assert all("/genebuild/" in path for path in paths)
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='genebuild')
+        assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/community/geneset/2018_10'
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='assembly')
+        assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/genome'
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='variation')
+        assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/community/variation/2018_10'
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='homologies')
+        assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/community/homology/2018_10'
+        with pytest.raises(TypeNotFoundException):
+            genome_adapter.get_public_path(genome_uuid, dataset_type='regulatory_features')
+            # assert path[0]['path'] == 'Saccharomyces_cerevisiae_S288c/GCA_000146045.2/ensembl/regulation'
 
     def test_default_public_path(self, test_dbs):
-        metadata_db = DBConnection(test_dbs['ensembl_genome_metadata'].dbc.url)
-        with metadata_db.session_scope() as session:
-            # Homo sapien GRCH38
-            genome = session.query(Genome).filter(Genome.genome_uuid == 'a7335667-93e7-11ec-a39d-005056b38ce3').first()
-            paths = genome.get_public_path(dataset_type='all')
-            assert len(paths) == 5
-            # assert all("/genebuild/" in path for path in paths)
-            path = genome.get_public_path(dataset_type='genebuild')
-            assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/geneset/2023_03'
-            path = genome.get_public_path(dataset_type='assembly')
-            assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/genome'
-            path = genome.get_public_path(dataset_type='variation')
-            assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/variation/2023_03'
-            path = genome.get_public_path(dataset_type='homologies')
-            assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/homology/2023_03'
-            path = genome.get_public_path(dataset_type='regulatory_features')
-            assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/regulation'
-
-    def test_organism_ensembl_name_compat(self, test_dbs):
-        """ Validate that we can still yse ensembl_name in queries from SQLAlchemy
-        This test will fail when we remove the ORM column for good
-        """
-        metadata_db = DBConnection(test_dbs['ensembl_genome_metadata'].dbc.url)
-        with metadata_db.session_scope() as session:
-            ensembl_name = session.query(Organism).filter(Organism.ensembl_name == 'SAMN12121739').first()
-            biosample_id = session.query(Organism).filter(Organism.biosample_id == 'SAMN12121739').first()
-            assert ensembl_name.organism_uuid == biosample_id.organism_uuid
-
+        genome_adapter = GenomeAdaptor(test_dbs['ensembl_genome_metadata'].dbc.url, test_dbs['ncbi_taxonomy'].dbc.url)
+        genome_uuid = 'a7335667-93e7-11ec-a39d-005056b38ce3'
+        # Homo sapien GRCH38
+        paths = genome_adapter.get_public_path(genome_uuid, dataset_type='all')
+        assert len(paths) == 5
+        # assert all("/genebuild/" in path for path in paths)
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='genebuild')
+        assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/geneset/2023_03'
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='assembly')
+        assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/genome'
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='variation')
+        assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/variation/2023_03'
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='homologies')
+        assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/homology/2023_03'
+        path = genome_adapter.get_public_path(genome_uuid, dataset_type='regulatory_features')
+        assert path[0]['path'] == 'Homo_sapiens/GCA_000001405.29/ensembl/regulation'
